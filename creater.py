a111998499e1cbe162bec3312a790d7e77807761
knTkd <gloel.loe2@gmail.com>
2021-03-18 19:52:26 +0900

    ハンバーガーメニューが透けてる

--- creater.py
+++ creater.py
@@ -8,6 +8,8 @@ class Appender:
     def __init__(self, input_filepath='input.txt', origin_filepath='origin.html', created_filepath='created/document.html'):
         # 埋め込むときの元となるインデント
         self.indent = 1
+        
+        self.index_sep = '-'
 
         # 各ファイルの名前をセット
         self.input_file = input_filepath
@@ -20,7 +22,6 @@ class Appender:
         self.created_lines = self.origin_lines[::]
         # inputファイルをリストとして読み込み
         self.input_lines = self.readlines(self.input_file)
-
         # spaceをすべて半角に変換し、文末の\nを消す
         self.input_lines = [il.replace('　', ' ')[:-1] for il in self.input_lines]
         # 空行もしくはspaceだけの行を排除する
@@ -42,32 +43,74 @@ class Appender:
         # 1-2-1のような索引をつくる(str)
         self.indexes = self.levels2indexes(self.indent_levels)
         # 一番上のインデントレベルは索引なしにする それ以外の索引は１つレベルを下げる
+        '''
         for i in range(len(self.indexes)):
             if self.indexes[i] == '1':
                 self.indexes[i] = ''
             else:
                 self.indexes[i] = self.indexes[i][2:]
+        '''
         # =======================================================================
+
+        # 目次をoriginファイルの何行目に埋め込むかを探す
+        self.insert_line_table = self.find_insert_line_table()
 
         # originファイルの何行目に埋め込むかを探す
         self.insert_line = self.find_insert_line()
 
 
     def go(self):
+        
+        # 目次を作る
+        table_contents = self.make_table()
+
+        # raw_lineなどをもとにhtmlを作成
         html_lines = self.convert_to_html()
 
-        # append all
-        self.append_html_lines(html_lines)
+        table_contents = list(map(self.prepare_append, table_contents))
+        html_lines = list(map(self.prepare_append, html_lines))
+        if self.insert_line_table > self.insert_line:
+            table_contents, html_lines = html_lines, table_contents
+            self.insert_line, self.insert_line_table = self.insert_line_table, self.insert_line
+        
+        self.created_lines = self.created_lines[:self.insert_line_table] + table_contents + self.created_lines[self.insert_line_table:self.insert_line] + html_lines + self.created_lines[self.insert_line:]
 
         # createdファイルに書き込む
         with open(self.created_file, mode='w') as f:
             f.writelines(self.created_lines)
-    
-    def append_html_lines(self, html_lines):
-        for html_line in html_lines:
-            append_line = self.nspace(self.indent) + html_line + '\n'
-            self.created_lines.insert(self.insert_line, append_line)
-            self.insert_line += 1
+
+    def prepare_append(self, text):
+        return self.nspace(self.indent) + text + '\n'
+
+    def make_table(self):
+        # self.indent_level と self.raw_linesを使って目次のhtmlを作る
+        r = list()
+        r_idx = 0
+        r_idx, r = self.elem_insert(r_idx, r, 'ul')
+        indent_levels = [-1] + self.indent_levels
+        for i, (cur, pre) in enumerate(zip(indent_levels[1:], indent_levels[:-1])):
+            if self.is_local_bottoms[i] or self.is_codes[i]:
+                continue
+            if cur > pre:
+                r_idx, r = self.elem_insert(r_idx, r, 'ul')
+                r_idx, r = self.elem_insert(r_idx, r, 'li')
+            else:
+                r_idx += (pre - cur + 1)
+                r_idx, r = self.elem_insert(r_idx, r, 'li')
+            
+            a_str = '<a href=#' + self.indexes[i] + '>' + self.raw_lines[i] + '</a>'
+            r.insert(r_idx, a_str)
+            r_idx += 1
+        return r
+
+    def elem_insert(self, idx, lines, tag):
+        # linesにidx位置で<tag></tag>をいい感じにインサートする
+        # 戻り値は新しいidxと入れたリスト
+        # elem_insert(1, ['<ul>', '</ul>'], 'li')
+        # (2, ['<ul>', '<li>', '</li>' '</ul>'])  returned
+        lines.insert(idx, '</' + tag + '>')
+        lines.insert(idx, '<' + tag + '>')
+        return (idx + 1, lines)
 
     def convert_to_html(self):
         # 各リストからhtmlの要素を作って返す
@@ -80,22 +123,22 @@ class Appender:
                 r.append(html_line)
                 continue
 
-            if self.indexes[i] == '':
+            if self.indexes[i].isdigit():
                 html_line = self.addhr(html_line)
             else:
                 # 1-2-1. なになに〜とする
-                html_line = self.indexes[i] + '. ' + html_line
-            html_line = self.raw2html(html_line, self.indent_levels[i])
+                html_line = self.indexes[i][self.indexes[i].find(self.index_sep) + 1:] + '. ' + html_line
+            html_line = self.raw2html(html_line, self.indent_levels[i], id=self.indexes[i])
             r.append(html_line)
         return r
 
     def raw2code(self, text):
         return '<pre class="code">' + text + '</pre>'
 
-    def raw2html(self, text, indent_level):
+    def raw2html(self, text, indent_level, id=''):
         # 生テキストをhtmlに変換する関数
         h_elm = min(6, indent_level + 1)
-        text = '<h' + str(h_elm) + '>' + text + '</h' + str(h_elm) + '>'
+        text = '<h' + str(h_elm) + ' id=' + id + '>' + text + '</h' + str(h_elm) + '>'
         return text
 
     def addhr(self, text):
@@ -122,8 +165,7 @@ class Appender:
 
     def numlist2index(self, nlist):
         # [1, 2, 4] :  '1-2-4'
-        sep = '-'
-        return sep.join(map(str, nlist))
+        return self.index_sep.join(map(str, nlist))
         
 
     def levels2bottoms(self, indent_levels):
@@ -131,10 +173,11 @@ class Appender:
         on_decrease = True
         last_down_idx = 1
         r = [False] * len(indent_levels)
-        indent_levels.append(-1)  # 一番最後に強制的に上げることで未処理がないように
-        for i, (cur, pre) in enumerate(zip(indent_levels[1:], indent_levels[:-1]), 1):
+        levels = indent_levels + [-1] # 一番最後に強制的に上げることで未処理がないように
+        for i, (cur, pre) in enumerate(zip(levels[1:], levels[:-1]), 1):
             if cur < pre and on_decrease:
                 on_decrease = False
+                # 最後に下がったところから今の地点までを全部Trueに
                 for j in range(last_down_idx, i):
                     r[j] = True
             elif cur > pre:
@@ -155,49 +198,10 @@ class Appender:
         return r
         # return self.n2br(self.pruning(input_line))
 
-    def make_table(self):
-        # self.indent_level と self.raw_linesを使って目次のhtmlを作る
-        r = list()
-        r.append('<div class="table">')
-        r.append('  <ol>')
-        r.append('    <li><a href="#' + '')
-        
-
-        r.append('  </ol>')
-        r.append('</div>')
-        return r
-    
-    def convert_input_lines(self):
-        # self.input_linesを埋め込む文字列としてのhtmlに変換する
-        
-        pre_indent_level = -1  # 前のインデントレベル
-        index_list = [1] * 6   # 今のインデックスの番号をリストで持つ
-        r = list()
-        for indent_level, input_line, local_bottom in zip(self.indent_levels, self.input_lines, self.local_bottom):
-            index_list = self.next_index_list(pre_indent_level, indent_level, index_list)
-            raw_line = self.input2raw(input_line)
-            if raw_line.find('code ') == 0:
-                html_line = self.raw2code(raw_line[5:])
-                raw_line = ''
-            elif local_bottom:
-                html_line = raw_line
-                raw_line = ''
-            else:
-                raw_line = self.index_list2str(indent_level, index_list) + raw_line
-                html_line = self.raw2html(raw_line, indent_level)
-            r.append(html_line)
-
-            # 目次用にraw_lineを取っておく (コードとボトムは空文字として)
-            self.raw_lines.append(raw_line)
-
-            pre_indent_level = indent_level
-        return r
-
     def n2br(self, text):
         # \n -> <br>
         raw_text = repr(text)[1:-1]  # rawにしてるので前後に'が付く
         return raw_text.replace(r'\\n', '<br>')
-    
     
     def prun_head_space(self, text):
         # 先頭のの空白を捨てる(pruningは剪定のこと)
@@ -217,15 +221,20 @@ class Appender:
 
     def find_insert_line(self):
         for line, text in enumerate(self.origin_lines):
-            #if '</body>' in text:
             if '<!-- 生成したコード -->' in text:
                 return line + 1
         print(f'error: origin.htmlファイル内に<!-- 生成したコード -->が見当たりません')
         exit(1)
 
+    def find_insert_line_table(self):
+        for line, text in enumerate(self.origin_lines):
+            if '<!-- 目次 -->' in text:
+                return line + 1
+        print(f'error: origin.htmlファイル内に<!-- 目次 -->が見当たりません')
+        exit(1)
+
     def readlines(self, filepath):
         # filepathをもらって、中身を行ごとのリストで返す
-        r = None
         try:
             with open(filepath) as f:
                 r = f.readlines()
@@ -240,11 +249,12 @@ def main():
 
     kwargs = dict()
 
+    basedir = os.path.dirname(os.path.abspath(__file__))
+
     # typ = [('てきすとふぁいる', '*.txt')]
     typ = [('', '*')]
-    kwargs['input_filepath'] = filedialog.askopenfilename(filetypes=typ, initialdir='.')
-
-    basedir = os.path.dirname(os.path.abspath(__file__))
+    # kwargs['input_filepath'] = filedialog.askopenfilename(filetypes=typ, initialdir='.')
+    kwargs['input_filepath'] = os.path.join(basedir, 'input.txt')
 
     kwargs['origin_filepath'] = os.path.join(basedir, 'origin.html')
     created_filename = input('生成されるhtmlファイルの名前を拡張子なしで入力してください（そのままエンターを押すとデフォルトのdocument.htmlで保存されます）')
